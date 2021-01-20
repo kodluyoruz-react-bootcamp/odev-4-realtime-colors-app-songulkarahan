dc94374d76d0f66b8255af609adcff63542618e0
songulkarahan <songul.karahan@bil.omu.edu.tr>
2021-01-20 05:36:41 +0300

    Import socket.io-client && modified client content

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 const app = require("express")();
 const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("connected...");
 });
 
 http.listen(3000, () => {

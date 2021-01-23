9d0fa6bd3eac96da1b03b4403601ed174db28fb6
songulkarahan <songul.karahan@bil.omu.edu.tr>
2021-01-24 01:14:16 +0300

    Created clint-side && server-side socket connections

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,16 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+  res.end("Helloo");
 });
 
 io.on("connection", (socket) => {
   console.log("connected...");
+
+  socket.on("color", (color) => {
+    console.log(color);
+    io.emit("received-color", color);
+  });
 });
 
 http.listen(3000, () => {

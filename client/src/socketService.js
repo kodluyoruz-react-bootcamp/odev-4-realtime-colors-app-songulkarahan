25ece8d36ec655a1a9ec3837a1855dec808f78af
songulkarahan <songul.karahan@bil.omu.edu.tr>
2021-01-25 18:09:56 +0300

    added netlify link

--- client/src/socketService.js
+++ client/src/socketService.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 let socket;
 
 export const initSocket = () => {
-  socket = io("http://localhost:3000", {
+  socket = io("https://peaceful-cliffs-29428.herokuapp.com/", {
     transports: ["websocket"],
   });
 

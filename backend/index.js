f08bcbfe16afee4d995835b25a1817fe14ac1d5b
songulkarahan <songul.karahan@bil.omu.edu.tr>
2021-01-25 17:22:08 +0300

    configure

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.end("Helloo");

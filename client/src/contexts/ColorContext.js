a0cf611b9e740bc067f33032c76c3c893687550d
songulkarahan <songul.karahan@bil.omu.edu.tr>
2021-01-25 20:38:36 +0300

    Changed color

--- client/src/contexts/ColorContext.js
+++ client/src/contexts/ColorContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 const ColorContext = createContext(null);
 
 export const ColorProvider = ({ children }) => {
-  const [color, setColor] = useState("#fff");
+  const [color, setColor] = useState("#F77E7E");
 
   const values = {
     color,
